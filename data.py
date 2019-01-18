df5bec51fb5a7c4a7e8ce20e6a07b7c75ccbda22
ildarin22 <ildarimp@list.ru>
2019-01-18 19:19:07 +0300

    180119

--- data.py
+++ data.py
@@ -45,7 +45,7 @@ def adjustData(img,mask,flag_multi_class,num_class):
 
 
 
-def trainGenerator(batch_size,train_path,image_folder,mask_folder,aug_dict,image_color_mode = "grayscale",
+def trainGenerator(batch_size,train_path,image_folder,mask_folder,aug_dict,image_color_mode = "grayscale",   #grayscale
                     mask_color_mode = "grayscale",image_save_prefix  = "image",mask_save_prefix  = "mask",
                     flag_multi_class = False,num_class = 2,save_to_dir = None,target_size = (256,256),seed = 1):
     '''
--- main.py
+++ main.py
@@ -11,12 +11,15 @@ data_gen_args = dict(rotation_range=0.2,
                     zoom_range=0.05,
                     horizontal_flip=True,
                     fill_mode='nearest')
-myGene = trainGenerator(2,'data/membrane/train','image','label',data_gen_args,save_to_dir = None)
+
+# myGene = trainGenerator(2,'data/bottles/train','image','label',data_gen_args,save_to_dir = None)
+
+myGene_mem = trainGenerator(2, 'data/membrane/train', 'image', 'label', data_gen_args, save_to_dir=None)
 
 model = unet()
 model_checkpoint = ModelCheckpoint('unet_membrane.hdf5', monitor='loss',verbose=1, save_best_only=True)
-model.fit_generator(myGene,steps_per_epoch=300,epochs=1,callbacks=[model_checkpoint])
+model.fit_generator(myGene_mem,steps_per_epoch=50,epochs=5,callbacks=[model_checkpoint])
 
-testGene = testGenerator("data/membrane/test")
-results = model.predict_generator(testGene,30,verbose=1)
-saveResult("data/membrane/test",results)
+# testGene = testGenerator("data/membrane/test")
+# results = model.predict_generator(testGene,30,verbose=1)
+# saveResult("data/membrane/test",results)
